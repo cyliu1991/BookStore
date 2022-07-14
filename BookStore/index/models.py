07a621d145ea295595d857e441c32388568912e0
liuchunyuan <liuchunyuan@haier.com>
2022-07-14 17:42:03 +0800

    添加出版社与图书之间的一对多关系映射

--- BookStore/index/models.py
+++ BookStore/index/models.py
@@ -2,18 +2,23 @@ from django.db import models
 
 
 # Create your models here.
+class PubName(models.Model):
+    pubname = models.CharField('名称', max_length=255, unique=True)
+
+
 class Book(models.Model):
     title = models.CharField(max_length=30, unique=True, verbose_name='书名')
-    public = models.CharField(max_length=50,verbose_name='出版社')
+    # public = models.CharField(max_length=50,verbose_name='出版社')
     price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name='定价')
 
     def default_price(self):
         return '￥30'
 
     retail_price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name='零售价', default=default_price)
+    pub = models.ForeignKey(to=PubName, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
-        return "title: %s pub: %s price: %s" % (self.title, self.public, self.price)
+        return "title: %s pub: %s price: %s" % (self.title, self.pub, self.price)
 
 
 class Author(models.Model):
