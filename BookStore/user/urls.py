f594ce69f990c9e958f9e21296056d87c001d5a6
liuchunyuan <liuchunyuan@haier.com>
2022-08-17 16:26:21 +0800

    add register fuction

--- BookStore/user/urls.py
+++ BookStore/user/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from user import views
 urlpatterns = [
     path('login/', views.login_view),
-    path('logout/', views.logout_view)
+    path('logout/', views.logout_view),
+    path('reg/', views.reg_view)
 ]
--- BookStore/user/views.py
+++ BookStore/user/views.py
@@ -1,10 +1,10 @@
 import hashlib
 
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-from index.models import UserInfo
+from user.models import User
 
 
 def login_view(request):
@@ -24,7 +24,7 @@ def login_view(request):
         if not username or not password:
             error = '你输入的用户名或者密码错误！'
             return render(request, 'user/login.html', locals())
-        users = UserInfo.objects.filter(username=username, password=password)
+        users = User.objects.filter(username=username, password=password)
         if not users:
             error = '用户不存在或用户密码输入错误!!'
             return render(request, 'user/login.html', locals())
@@ -46,3 +46,49 @@ def logout_view(request):
     if 'username' in request.COOKIES:
         resp.delete_cookie('username')
     return resp
+
+def reg_view(request):
+    if request.method == "GET":
+        return render(request, 'user/register.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        if not username:
+            username_error = "请输入正确的用户名"
+            return render(request, 'user/register.html', locals())
+        password_1 = request.POST.get('password_1')
+        m = hashlib.md5()
+        m.update(password_1.encode())
+        password_m1 = m.hexdigest()
+        print(password_m1)
+
+        password_2 = request.POST.get('password_2')
+        m = hashlib.md5()
+        m.update(password_2.encode())
+        password_m2 = m.hexdigest()
+        print(password_m2)
+
+        if not password_m1 or not password_m2:
+            password_1_error = '请输入正确的密码'
+            return render(request, 'user/register.html', locals())
+
+        if password_m2 != password_m2:
+            password_2_error = '两次密码不一致'
+            return render(request, 'user/register.html', locals())
+
+        try:
+            old_user = User.objects.get(username=username)
+            username_error = '用户已被注册！'
+            return render(request, 'user/register.html', locals())
+        except Exception as e:
+            print('%s是可用用户名——%s' % (username, e))
+            try:
+                user = User.objects.create(username=username, password=password_m1)
+                html = """
+                注册成功 点击<a href='/index/'>进入首页</a>
+                """
+                request.session['username'] = username
+                return HttpResponse(html)
+            except Exception as e:
+                print(e)
+                username_error = '该用户名已被占用'
+                return render(request, 'user/register.html', locals())
